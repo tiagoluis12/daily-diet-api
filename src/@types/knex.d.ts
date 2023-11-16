b03d018a5a9fcb4d2b468cf3626902a1b64da03e
“tiagoluis12” <tiago7164@hotmail.com>
2023-11-16 10:27:28 +0000

    Implemented: Knex typing

--- src/@types/knex.d.ts
+++ src/@types/knex.d.ts
@@ -1,14 +1,9 @@
 // eslint-disable-next-line
 import { knex } from 'knex'
+import { User } from './user'
 
 declare module 'knex/types/tables' {
   export interface Tables {
-    transactions: {
-      id: string
-      title: string
-      amount: number
-      created_at: string
-      session_id?: string
-    }
+    users: User
   }
 }
