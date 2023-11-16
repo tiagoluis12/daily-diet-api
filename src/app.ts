92d2d966549b3e847356796dc0d71c5bf69f90e5
“tiagoluis12” <tiago7164@hotmail.com>
2023-11-16 13:37:22 +0000

    Implemented Urls

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,15 @@ import cookie from '@fastify/cookie'
 
 import { env } from './env'
 import { mealsRoutes } from './routes/meals'
+import { usersRoutes } from './routes/users'
 
 export const app = fastify()
 
 app.register(cookie)
+
+app.register(usersRoutes, {
+  prefix: 'users',
+})
 
 app.register(mealsRoutes, {
   prefix: 'meals',
